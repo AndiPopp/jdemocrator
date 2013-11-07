83ce21d6cd17435cc80b98c4970dda9c2f4de238
Andi <andreas.popp@piratenpartei-bayern.de>
2013-11-07 18:45:04 +0100

    folders

---
+++ src/de/inpiraten/jdemocrator/TAN/AbstractTAN.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.inpiraten.jdemocrator.TAN;
+
+/**
+ * @author andi
+ *
+ */
+public abstract class AbstractTAN {
+	
+	/**
+	 * A string to describe the type of the key
+	 */
+	String type;
+	
+	/**
+	 * The number of the election the TAN is viable for
+	 */
+	int electionNumber;
+}
---
+++ src/de/inpiraten/jdemocrator/TAN/XORTAN.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package de.inpiraten.jdemocrator.TAN;
+
+public class XORTAN extends AbstractTAN {
+
+	/**
+	 * The Index of the TAN
+	 */
+	int tanIndex;
+	
+	/**
+	 * The key of the 
+	 */
+	int key;
+}
