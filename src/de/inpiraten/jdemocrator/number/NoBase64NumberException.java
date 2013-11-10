1bfc796a76be0c6bfbbe23a3187363efd15371db
Andi <andreas.popp@piratenpartei-bayern.de>
2013-11-10 15:11:26 +0100

    trying to add missing files

---
+++ src/de/inpiraten/jdemocrator/number/NoBase64NumberException.java
@@ -0,0 +1,15 @@
+package de.inpiraten.jdemocrator.number;
+
+
+public class NoBase64NumberException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5202213627484117649L;
+
+	public NoBase64NumberException(String string) {
+		super(string);
+	}
+
+}
